eb76544a07fb4fbb084c8b9dc354a0e8ef1ac455
MANOJ BHARGAV <MANOJ BHARGAV@DESKTOP-51JP16V>
2020-03-03 22:29:40 +0530

    cmt

---
+++ collections/src/main/java/com/collection/initiallist.java
@@ -0,0 +1,12 @@
+package com.collection;
+import java.util.*;
+public class initiallist {
+	public static void main(String args[])
+	{
+		List<String> list = Arrays.asList("1", "2", "3","4","5","6","7","8","0");
+		for(String x : list)
+		{
+			System.out.print(x+ " ");
+		}
+	}
+}
